2389fbd67ebd6bb2af873bfcbd000313c604621e
Philip Andrew <philip368320@gmail.com>
2019-03-07 16:25:16 +0800

    Case history improvements

--- src/components/case-history.js
+++ src/components/case-history.js
@@ -12,16 +12,32 @@ class NgoCase extends React.Component {
       <div className="col-md-12">
         <div className="border ngo-case">
           <div>
-          <a href="#">Help: Immigrating to Hong Kong</a>
+          <a href="#">Document authentication - Sri Lanka</a>
           <a href="#" className="float-right">Icons/Pictures</a>
           </div>
-          <div>
-            <p>Case description</p>
+          <div class="case-description">
+            <p>We require a Sri Lankan expert to authenticate an extract from the Information Book of of a Police Station in Negombo. </p>
           </div>
-          <div>
+
+
+          <div className="buttons">
           <button className="" type="submit">Details</button>
           <button className="" type="submit">Approve</button>
-          </div>          
+          </div>     
+
+          <div>
+              <div class="tagcloud03">
+                <ul>
+                  <li><a href="#">Immigration<span>20</span></a></li>
+                  <li><a href="#">tag<span>20</span></a></li>
+                  <li><a href="#">tag<span>20</span></a></li>
+                  <li><a href="#">tag<span>20</span></a></li>
+                  <li><a href="#">tag<span>20</span></a></li>
+                  <li><a href="#">Lorem ipsum dolor sit amet<span>20</span></a></li>
+                </ul>
+              </div>            
+          </div>
+     
         </div>
         </div>
       </div>
