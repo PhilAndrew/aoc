3046c9de03efff12b5ed351526c61ef5793ab82e
Philip Andrew <philip368320@gmail.com>
2019-03-07 16:30:25 +0800

    NGO Case is now template file

--- src/components/case-history.js
+++ src/components/case-history.js
@@ -3,47 +3,7 @@ import GuestLayout from './guest-layout';
 import localStyles from './local-styles.css';
 import Select from 'react-select';
 import CatDog from '../resources/img/phil.jpeg';
-
-class NgoCase extends React.Component {
-  
-  render() {
-    return (
-      <div class="row">
-      <div className="col-md-12">
-        <div className="border ngo-case">
-          <div>
-          <a href="#">Document authentication - Sri Lanka</a>
-          <a href="#" className="float-right">Icons/Pictures</a>
-          </div>
-          <div class="case-description">
-            <p>We require a Sri Lankan expert to authenticate an extract from the Information Book of of a Police Station in Negombo. </p>
-          </div>
-
-
-          <div className="buttons">
-          <button className="" type="submit">Details</button>
-          <button className="" type="submit">Approve</button>
-          </div>     
-
-          <div>
-              <div class="tagcloud03">
-                <ul>
-                  <li><a href="#">Immigration<span>20</span></a></li>
-                  <li><a href="#">tag<span>20</span></a></li>
-                  <li><a href="#">tag<span>20</span></a></li>
-                  <li><a href="#">tag<span>20</span></a></li>
-                  <li><a href="#">tag<span>20</span></a></li>
-                  <li><a href="#">Lorem ipsum dolor sit amet<span>20</span></a></li>
-                </ul>
-              </div>            
-          </div>
-     
-        </div>
-        </div>
-      </div>
-    )
-  }
-}
+import NgoCase from './ngo-case';
 
 export default () => (
   <GuestLayout>
