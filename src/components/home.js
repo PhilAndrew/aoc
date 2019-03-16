e7ee6fd156a01d95cbd582cf0bc712b84212c906
Philip Andrew <philip368320@gmail.com>
2019-03-16 17:10:25 +0800

    Style stuff

--- src/components/home.js
+++ src/components/home.js
@@ -8,6 +8,11 @@ import LandingBanner from './landing-banner';
 export default () => (
   <GuestLayout>
     <section className="hero">
+    <div style={{ opacity: '0.5', height: '410px', overflow: 'hidden', position: 'absolute', width: '100%' }}>
+      <svg viewBox="0 0 500 150" preserveAspectRatio="none" style={{ height: '100%', width: '100%;' }}>
+        <path d="M0.00,92.27 C216.83,192.92 304.30,8.39 500.00,109.03 L500.00,0.00 L0.00,0.00 Z" style={{ stroke: 'none', fill: '#99D5EA' }}></path>
+      </svg>
+    </div>
       <div className="">
         <div className="container">
           <section className="row">
