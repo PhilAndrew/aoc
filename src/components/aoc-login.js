fb63e700b5f6a15180259fb612318ac3ea4805d5
Philip Andrew <philip368320@gmail.com>
2019-03-17 14:29:03 +0800

    ...

--- src/components/aoc-login.js
+++ src/components/aoc-login.js
@@ -110,9 +110,9 @@ export default class AocLogin extends React.Component {
         this.setState({
           loggedIn: true,
         });
-      } 
       }
-  
+      }
+
 
       updateUserNameValue(evt) {
         this.setState({
@@ -142,13 +142,13 @@ export default class AocLogin extends React.Component {
             <p>Welcome NGO user</p>
             <button type="submit" className="button is-link margin-right-5px login-button" onClick={e => this.logout(e)}>Logout</button>
             </div>)
-      else    
+      else
       if (this.state.userNameValue === 'coordinator')
         return (<div>
             <p>Welcome Coordinator user</p>
             <button type="submit" className="button is-link margin-right-5px login-button" onClick={e => this.logout(e)}>Logout</button>
             </div>)
-      else    
+      else
       if (this.state.userNameValue === 'lawyer')
         return (<div>
             <p>Welcome Lawyer user</p>
@@ -161,18 +161,17 @@ export default class AocLogin extends React.Component {
     return (
         <div>
         <section className="row">
-        <div className="col-md-9 section_title animated wow fadeInUp">
-        <div className="row">
-        <div className="col-md-9">
-        <h1 style={{ fontSize: '23pt'}}>Access Our Community</h1>
-        <div style={{ backgroundImage: `url(${HomepageJpeg})`, opacity: '0.8', backgroundPosition: 'right -280px top -290px', boxShadow: '0 0 3px 3px rgba(0, 0, 0, 0.9) inset', height: '200px' }} />
-        </div>
-        <div className="col-md-3">
-                <h2>User login.</h2>
-                <p>Login with user name as ngo or coordinator or lawyer with any password.</p>
+          <div className="col-md-9 section_title animated wow fadeInUp">
+            <div className="row">
+              <div className="col-md-12">
+                <h1 style={{ float: 'left', fontSize: '23pt'}}>Access Our Community</h1>
+                <div style={{ float: 'right', width: '400px', backgroundImage: `url(${HomepageJpeg})`, opacity: '0.8', backgroundPosition: 'right -460px top -340px', boxShadow: '0 0 3px 3px rgba(0, 0, 0, 0.9) inset', height: '220px' }} />
+                <p style={{ marginTop: '40px' }}>
+                Hello there. Hello there. Hello there. Hello there. Hello there. Hello there. Hello there. Hello there.
+                </p>
                 </div>
-              </div>     
-              </div>     
+              </div>
+              </div>
         <div className="col-md-3 user-login-area">
         <div className="field">
                 <label className="label" htmlFor="username">
@@ -216,6 +215,35 @@ export default class AocLogin extends React.Component {
         </div>
 
         </section>
+
+        <section className="row" style={{ marginTop: '0px'}}>
+        <div className="col-md-4" style={{ padding: '20px'}}>
+        <div className="row user-login-area" style={{ height: '200px' }}>
+        <p>
+        <h1 className="homepageSubtitle">For Lawyer</h1>
+        <div><p>Hello text goes here.</p></div>
+        </p>
+        </div>
+        </div>
+
+        <div className="col-md-4" style={{ padding: '20px'}}>
+        <div className="row user-login-area" style={{ height: '200px' }}>
+        <p>
+        <h1 className="homepageSubtitle">For Non-Profit</h1>
+        <div><p>Hello text goes here.</p></div>
+        </p>
+        </div>
+        </div>
+        <div className="col-md-4" style={{ padding: '20px'}}>
+        <div className="row user-login-area" style={{ height: '200px' }}>
+        <p>
+        <h1 className="homepageSubtitle">For Law Firm coordinator</h1>
+        <div><p>Hello text goes here.</p></div>
+        </p>
+        </div>
+        </div>
+        </section>
+
         </div>
     );
   }
